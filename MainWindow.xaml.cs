bce6e6614a1f331b5edc4580bcabf6798ceafb71
Yao Peng <t-yaope@mds.microsoft.com>
2016-06-14 10:03:45 +0300

    face detect by kinect infrare camera

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,415 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.InfraredBasics
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.ProjectOxford.Face;
+    using Microsoft.ProjectOxford.Face.Contract;
+    using System.Windows.Shapes;
+    using System.Windows.Controls;
+    using System.Diagnostics;
+    /// <summary>
+    /// Interaction logic for the MainWindow
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Maximum value (as a float) that can be returned by the InfraredFrame
+        /// </summary>
+        private const float InfraredSourceValueMaximum = (float)ushort.MaxValue;
+        
+        /// <summary>
+        /// The value by which the infrared source data will be scaled
+        /// </summary>
+        private const float InfraredSourceScale = 0.75f;
+
+        /// <summary>
+        /// Smallest value to display when the infrared data is normalized
+        /// </summary>
+        private const float InfraredOutputValueMinimum = 0.01f;
+
+        /// <summary>
+        /// Largest value to display when the infrared data is normalized
+        /// </summary>
+        private const float InfraredOutputValueMaximum = 1.0f;
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor kinectSensor = null;
+
+        /// <summary>
+        /// Reader for infrared frames
+        /// </summary>
+        private InfraredFrameReader infraredFrameReader = null;
+
+        /// <summary>
+        /// Description (width, height, etc) of the infrared frame data
+        /// </summary>
+        private FrameDescription infraredFrameDescription = null;
+
+        /// <summary>
+        /// Bitmap to display
+        /// </summary>
+        private WriteableBitmap infraredBitmap = null;
+
+        /// <summary>
+        /// Current status text to display
+        /// </summary>
+        private string statusText = null;
+
+        /// <summary>
+        /// Face service client
+        /// </summary>
+        private IFaceServiceClient _faceServiceClient = null;
+
+        /// <summary>
+        /// Face API key
+        /// </summary>
+        private string faceAPIKey = "f8c7e6233add4c7c9205e8b2c1e7c6fa";
+
+        /// <summary>
+        /// enable/disable face detection, default is false
+        /// </summary>
+        private bool enableFaceDetect = false;
+
+        /// <summary>
+        /// process the face detection one by one
+        /// </summary>
+        bool faceProcessing = false;
+
+        Rectangle faceBoundingBox = new Rectangle();
+        
+
+        /// <summary>
+        /// encoder to save the frame to a memorystream as jpeg
+        /// </summary>
+        //private BitmapEncoder faceDetectEncoder = new JpegBitmapEncoder();
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            // get the kinectSensor object
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // open the reader for the depth frames
+            this.infraredFrameReader = this.kinectSensor.InfraredFrameSource.OpenReader();
+
+            // wire handler for frame arrival
+            this.infraredFrameReader.FrameArrived += this.Reader_InfraredFrameArrived;
+
+            // get FrameDescription from InfraredFrameSource
+            this.infraredFrameDescription = this.kinectSensor.InfraredFrameSource.FrameDescription;
+
+            // create the bitmap to display
+            // this.infraredBitmap = new WriteableBitmap(this.infraredFrameDescription.Width, this.infraredFrameDescription.Height, 96.0, 96.0, PixelFormats.Gray32Float, null);
+            this.infraredBitmap = new WriteableBitmap(this.infraredFrameDescription.Width, this.infraredFrameDescription.Height, 96.0, 96.0, PixelFormats.Gray32Float, null);
+
+            // set IsAvailableChanged event notifier
+            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+
+            // open the sensor
+            this.kinectSensor.Open();
+
+            // set the status text
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+                                                            : Properties.Resources.NoSensorStatusText;
+
+            // use the window object as the view model in this simple example
+            this.DataContext = this;
+
+            // Initialize the face service client
+            _faceServiceClient = new FaceServiceClient(faceAPIKey);
+
+            faceBoundingBox.Stroke = new SolidColorBrush(Colors.Red);
+
+            // initialize the components (controls) of the window
+            this.InitializeComponent();
+
+
+        }
+
+        /// <summary>
+        /// INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets the bitmap to display
+        /// </summary>
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return this.infraredBitmap;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current status text to display
+        /// </summary>
+        public string StatusText
+        {
+            get
+            {
+                return this.statusText;
+            }
+
+            set
+            {
+                if (this.statusText != value)
+                {
+                    this.statusText = value;
+
+                    // notify any bound elements that the text has changed
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("StatusText"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (this.infraredFrameReader != null)
+            {
+                // InfraredFrameReader is IDisposable
+                this.infraredFrameReader.Dispose();
+                this.infraredFrameReader = null;
+            }
+
+            if (this.kinectSensor != null)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+        }
+
+        /// <summary>
+        /// Handles the user clicking on the FaceDetect button
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void FaceDetectButton_Click(object sender, RoutedEventArgs e)
+        {
+            enableFaceDetect = !enableFaceDetect;
+        }
+
+        /// <summary>
+        /// Upload the frame and get the face detect result
+        /// </summary>
+        private async Task<FaceRectangle[]> UploadAndDetectFaces()
+        {
+            if (this.infraredBitmap == null)
+            {
+                return new FaceRectangle[0];
+            }
+
+            BitmapEncoder faceDetectEncoder = new JpegBitmapEncoder();
+            // create frame from the writable bitmap and add to encoder
+            faceDetectEncoder.Frames.Add(BitmapFrame.Create(this.infraredBitmap));
+
+            MemoryStream imageFileStream = new MemoryStream();
+
+            faceDetectEncoder.Save(imageFileStream);
+            imageFileStream.Position = 0;
+
+            if (!faceProcessing)
+            {
+                faceProcessing = true;
+                //Thread.Sleep(300);
+                try
+                {
+                    var faces = await _faceServiceClient.DetectAsync(imageFileStream);
+                    var faceRects = faces.Select(face => face.FaceRectangle);
+
+                    faceProcessing = false;
+                    return faceRects.ToArray();
+
+                }
+                catch (Exception)
+                {
+                    faceProcessing = false;
+                    return new FaceRectangle[0];
+                }
+            }
+            return new FaceRectangle[0];
+        }
+
+        /// <summary>
+        /// Handles the user clicking on the screenshot button
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void ScreenshotButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.infraredBitmap != null)
+            {
+                // create a png bitmap encoder which knows how to save a .png file
+                BitmapEncoder encoder = new PngBitmapEncoder();
+
+                // create frame from the writable bitmap and add to encoder
+                encoder.Frames.Add(BitmapFrame.Create(this.infraredBitmap));
+
+                string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+
+                string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+                string path = System.IO.Path.Combine(myPhotos, "KinectScreenshot-Infrared-" + time + ".png");
+
+                // write the new file to disk
+                try
+                {
+                    // FileStream is IDisposable
+                    using (FileStream fs = new FileStream(path, FileMode.Create))
+                    {
+                        encoder.Save(fs);
+                    }
+
+                    this.StatusText = string.Format(CultureInfo.CurrentCulture, Properties.Resources.SavedScreenshotStatusTextFormat, path);
+                }
+                catch (IOException)
+                {
+                    this.StatusText = string.Format(CultureInfo.CurrentCulture, Properties.Resources.FailedScreenshotStatusTextFormat, path);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the infrared frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_InfraredFrameArrived(object sender, InfraredFrameArrivedEventArgs e)
+        {
+            // InfraredFrame is IDisposable
+            using (InfraredFrame infraredFrame = e.FrameReference.AcquireFrame())
+            {
+                if (infraredFrame != null)
+                {
+                    // the fastest way to process the infrared frame data is to directly access 
+                    // the underlying buffer
+                    using (Microsoft.Kinect.KinectBuffer infraredBuffer = infraredFrame.LockImageBuffer())
+                    {
+                        // verify data and write the new infrared frame data to the display bitmap
+                        if (((this.infraredFrameDescription.Width * this.infraredFrameDescription.Height) == (infraredBuffer.Size / this.infraredFrameDescription.BytesPerPixel)) &&
+                            (this.infraredFrameDescription.Width == this.infraredBitmap.PixelWidth) && (this.infraredFrameDescription.Height == this.infraredBitmap.PixelHeight))
+                        {
+                            this.ProcessInfraredFrameData(infraredBuffer.UnderlyingBuffer, infraredBuffer.Size);
+                        }
+                    }
+                }
+
+                faceDetecting();
+            }
+        }
+
+        /// <summary>
+        /// Do the face detction and draw the rectangle of the faces
+        /// </summary>
+        private async void faceDetecting()
+        {
+            
+            if (enableFaceDetect)
+            {
+                Title = "Detecting...";
+               
+                FaceRectangle[] faceRects = await UploadAndDetectFaces();
+                Title = String.Format("Detection Finished. {0} face(s) detected", faceRects.Length);
+
+                if (faceRects.Length > 0)
+                {
+                    foreach (var faceRect in faceRects)
+                    {
+                        DrawRect(faceRect);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draw the face bounding box
+        /// </summary>
+        private void DrawRect(FaceRectangle face)
+        {
+            faceCanvas.Children.Clear();
+
+            faceBoundingBox.Width = face.Width;
+            faceBoundingBox.Height = face.Height;
+            faceBoundingBox.StrokeThickness = 2;
+
+            Canvas.SetLeft(faceBoundingBox, face.Left);
+            Canvas.SetTop(faceBoundingBox,face.Top);
+            faceCanvas.Children.Add(faceBoundingBox);
+        }
+
+        /// <summary>
+        /// Directly accesses the underlying image buffer of the InfraredFrame to 
+        /// create a displayable bitmap.
+        /// This function requires the /unsafe compiler option as we make use of direct
+        /// access to the native memory pointed to by the infraredFrameData pointer.
+        /// </summary>
+        /// <param name="infraredFrameData">Pointer to the InfraredFrame image data</param>
+        /// <param name="infraredFrameDataSize">Size of the InfraredFrame image data</param>
+        private unsafe void ProcessInfraredFrameData(IntPtr infraredFrameData, uint infraredFrameDataSize)
+        {
+            // infrared frame data is a 16 bit value
+            ushort* frameData = (ushort*)infraredFrameData;
+
+            // lock the target bitmap
+            this.infraredBitmap.Lock();
+
+            // get the pointer to the bitmap's back buffer
+            float* backBuffer = (float*)this.infraredBitmap.BackBuffer;
+
+            // process the infrared data
+            for (int i = 0; i < (int)(infraredFrameDataSize / this.infraredFrameDescription.BytesPerPixel); ++i)
+            {
+                // since we are displaying the image as a normalized grey scale image, we need to convert from
+                // the ushort data (as provided by the InfraredFrame) to a value from [InfraredOutputValueMinimum, InfraredOutputValueMaximum]
+                backBuffer[i] = Math.Min(InfraredOutputValueMaximum, (((float)frameData[i] / InfraredSourceValueMaximum * InfraredSourceScale) * (1.0f - InfraredOutputValueMinimum)) + InfraredOutputValueMinimum);
+            }
+
+            // mark the entire bitmap as needing to be drawn
+            this.infraredBitmap.AddDirtyRect(new Int32Rect(0, 0, this.infraredBitmap.PixelWidth, this.infraredBitmap.PixelHeight));
+
+            // unlock the bitmap
+            this.infraredBitmap.Unlock();
+        }
+
+        /// <summary>
+        /// Handles the event which the sensor becomes unavailable (E.g. paused, closed, unplugged).
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            // set the status text
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+                                                            : Properties.Resources.SensorNotAvailableStatusText;
+        }
+    }
+}
